ee2cfb9599128336e0b7975c62becfb2c752aa7b
Vlad Shchelkunov <vlad777.od@gmail.com>
2022-06-02 09:56:47 +0300

    mob-menu no-scroll

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,7 +1,8 @@
 (() => {
     const menuBtnRef = document.querySelector("[data-menu-button]");
     const mobileMenuRef = document.querySelector("[data-menu]");
-
+    const bodyRef = document.querySelector("body")
+    
     menuBtnRef.addEventListener("click", () => {
         const expanded =
             menuBtnRef.getAttribute("aria-expended") === "true" || false;
@@ -10,6 +11,7 @@
         menuBtnRef.setAttribute("aria-expanded", !expanded);
         
         mobileMenuRef.classList.toggle("is-open");
+        bodyRef.classList.toggle("no-scroll");
         
     });
 })();
